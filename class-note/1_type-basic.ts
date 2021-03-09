6daaa95b95dc7dd1a588df44d033d32badc8e014
김수진 <sjkim910115@gmail.com>
2021-03-09 22:38:37 +0900

    basic typescript

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,23 @@
+//var str = "hello";
+
+// ts 문자열 선언
+let str: string = "hello";
+let num: number = 10;
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk", 10];
+
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let adr: [string, number] = ["gnangnam", 100];
+
+// TS 객체
+let obj: object = {};
+
+let person: { name: string; age: number } = {
+  name: "thor",
+  age: 1000,
+};
+
+// 진위 값
+let show: boolean = true;
---
+++ class-note/2_function.ts
@@ -0,0 +1,22 @@
+// function add(a: number, b: number) {
+//   return a + b;
+// }
+
+// javascript
+function add(a, b) {
+  return a + b;
+}
+
+// typescript
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+add(10, 20);
+function log(a: string, b: string, c?: string) {
+  return a + b;
+}
+
+log("hello", "world", "why");
+log("hello", "world");
+//log("hello");
